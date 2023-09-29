379c12728ec1cedef9573b2f92d00b88630bdabe
NATALI-LEV <natalillev@gmail.com>
2023-09-29 14:03:47 +0300

    fixed issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends React.Component{
     state = { advice:'' };
+
+    componentDidMount(){
+        console.log('COMPONENT DID MOUNT');
+    }
 
     render(){
         return (
